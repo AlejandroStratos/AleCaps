f9743214439458ddc035758cfef360718484bde4
geronimolopez <titilopez_123@hotmail.com>
2024-06-24 20:02:16 -0300

    VISTAS RESPONSIVE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,63 +1,64 @@
-@extends('master') <!-- Extiende la vista maestra -->
+@extends('master')
 
 @section('head')
-    <!-- INTRODUCIR ACA LO QUE QUERES QUE SE AGREGUE DEL HEAD -->
-
     <style>
         .btn-custom {
             background-color: #5451EF;
             color: #FFFFFF;
             border-radius: 40px;
             display: block;
-            margin: 0 auto;
+            margin: 10px auto;
+            width: 100%;
+            max-width: 300px;
         }
-    </style>
 
-    <style>
+        @media (min-width: 768px) {
+            .btn-custom {
+                max-width: 300px;
+            }
+        }
+
         .alert-container {
-            max-width: 400px;
+            max-width: 90%;
             margin: 0 auto;
         }
     </style>
 @endsection
 
+@section('body')
+    <div class="container">
+        <h1 class="text-center">Centros de Atención Primaria de la Salud</h1>
 
-@section('body') <!-- INTRODUCIR ACA LO QUE QUERES QUE SE AGREGUE DEL BODY -->
-
-
-
-    <h1 class="text-center">Centros de Atención Primaria de la Salud</h1>
-
-    @if (session('success'))
-        <div class="alert-container">
-            <div class="alert alert-success">
-                {{ session('success') }}
+        @if (session('success'))
+            <div class="alert-container">
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
             </div>
-    @endif
+        @endif
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="text-center">
+            <a href="{{ route('familia.create') }}" class="d-block mb-3">
+                <button type="button" class="btn btn-custom">CREAR NUEVA ENCUESTA</button>
+            </a>
+
+            <a href="{{ route('encuesta.index') }}" class="d-block mb-3">
+                <button type="button" class="btn btn-custom">VER LISTADO DE ENCUESTAS</button>
+            </a>
+
+            <a href="{{ route('usuario.index') }}" class="d-block mb-3">
+                <button type="button" class="btn btn-custom">Ver USUARIOS</button>
+            </a>
         </div>
-    @endif
-
-    <a href="{{ route('familia.create') }}">
-        <button type="button" class="btn btn-custom">CREAR NUEVA ENCUESTA</button>
-    </a>
-
-    <br>
-
-    <a href="{{ route('encuesta.index') }}">
-        <button type="button" class="btn btn-custom">VER LISTADO DE ENCUESTAS</button>
-    </a>
-
-    <br>
-    <a href="{{ route('usuario.index') }}">
-        <button type="button" class="btn btn-custom">Ver USUARIOS</button>
-    </a>
-
+    </div>
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,54 +6,74 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CAPS</title>
 
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
+
+    <!-- Bootstrap JS and dependencies -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <!-- Link para agregar botones -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css">
-
+    <!-- Custom CSS -->
     <style>
         body {
             background-color: #a5edfd;
         }
-    </style>
 
-    <style>
         .alert-container {
-            max-width: 400px;
+            max-width: 90%;
             margin: 0 auto;
         }
-    </style>
 
-    <style>
-      <style>
-        .alert-container {
-            max-width: 400px;
-            margin: 0 auto;
-        }
-    </style>
-
-    <style>
         footer {
             text-align: center;
             padding: 10px;
+            margin-top: 20px;
         }
 
         .image-footer {
             display: flex;
             justify-content: center;
             align-items: center;
-            gap: 20px; /* Espacio entre las imágenes */
-            flex-wrap: wrap;
+            gap: 10px;
+            flex-wrap: nowrap;
         }
 
         .image-footer img {
-            max-width: 250px; /* Aumenta el tamaño máximo de las imágenes */
+            max-width: 150px;
             height: auto;
         }
-    </style>
+
+        @media (min-width: 768px) {
+            .alert-container {
+                max-width: 400px;
+            }
+
+            .image-footer img {
+                max-width: 200px;
+            }
+        }
+
+        @media (min-width: 1024px) {
+            .image-footer img {
+                max-width: 250px;
+            }
+        }
+
+        /* Additional responsive styles */
+        .btn-custom {
+            width: 100%;
+            max-width: 300px;
+            margin: 10px auto;
+        }
+
+        @media (min-width: 768px) {
+            .btn-custom {
+                max-width: 200px;
+            }
+        }
     </style>
 
     @yield('head') <!-- Aquí se insertará el contenido del head de cada vista -->
@@ -64,8 +84,8 @@
     <!-- Footer común -->
     <footer>
         <div class="image-footer">
-            <img src="{{ asset('img/LOGOAPS.png') }}" alt="Logo aps" class="logoaps">
-            <img src="{{ asset('img/LOGOMUNI.png') }}" alt="Logo muni" class="logomuni">
+            <img src="{{ asset('img/LOGOAPS.png') }}" alt="Logo APS" class="logoaps">
+            <img src="{{ asset('img/LOGOMUNI.png') }}" alt="Logo Muni" class="logomuni">
         </div>
     </footer>
 </body>
